9f59ee305f2c6f5b619d40d1531a24fb3c159964
Johannes K Becker <jkbecker@bu.edu>
2020-10-23 12:41:11 -0400

    more compact build script

--- build.sh
+++ build.sh
@@ -1,30 +1,16 @@
 set -e
 
-# Build minimal image
-echo "Building minimal image"
-cd pybombs-minimal
-docker build -t pybombs/pybombs-minimal:latest .
-pbversion=`docker run --rm -it pybombs/pybombs-minimal:latest pybombs --version | tr -d '\r'`
-docker tag pybombs/pybombs-minimal:latest pybombs/pybombs-minimal:$pbversion
-echo "Successfully built pybombs/pybombs-minimal (tagged latest and $pbversion)"
-echo ""
+images="minimal prefix commondeps"
+for img in $images; do
+    echo -e "\e[1;36mBuilding pybombs-$img ...\e[0m"
+    cd pybombs-$img
+    docker build -t pybombs/pybombs-$img:latest .
+    pbversion=`docker run --rm -it pybombs/pybombs-$img:latest pybombs --version | tr -d '\r'`
+    docker tag pybombs/pybombs-$img:latest pybombs/pybombs-$img:$pbversion
+    echo -e "\e[1;36mSuccessfully built pybombs/pybombs-$img (tagged latest and $pbversion).\e[0m"
+    cd ..
+    echo ""
+done
 
-# Build prefix image
-echo "Building prefix image"
-cd ../pybombs-prefix
-docker build -t pybombs/pybombs-prefix:latest .
-docker tag pybombs/pybombs-prefix:latest pybombs/pybombs-prefix:$pbversion
-echo "Successfully built pybombs/pybombs-prefix (tagged latest and $pbversion)"
-echo ""
-
-# Build commondeps image
-echo "Building commondeps image"
-cd ../pybombs-commondeps
-numthreads=`nproc || 2`
-docker build --build-arg makewidth=$numthreads -t pybombs/pybombs-commondeps:latest .
-docker tag pybombs/pybombs-commondeps:latest pybombs/pybombs-commondeps:$pbversion
-echo "Successfully built pybombs/pybombs-commondeps (tagged latest and $pbversion)"
-echo ""
-
-echo "Created the following images:"
+echo -e "\e[1;36mCreated the following images:\e[0m"
 docker images | grep pybombs/pybombs
