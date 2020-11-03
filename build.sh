300fcbd0b20ac53bc303f6b15db5e2b84bcf126b
Johannes K Becker <jkbecker@bu.edu>
2020-11-03 12:59:11 -0500

    better build tools

--- build.sh
+++ build.sh
@@ -1,16 +1,85 @@
+#!/bin/bash
 set -e
+set -o pipefail
 
-images="minimal prefix commondeps"
-for img in $images; do
-    echo -e "\e[1;36mBuilding pybombs-$img ...\e[0m"
-    cd pybombs-$img
-    docker build -t pybombs/pybombs-$img:latest .
-    pbversion=`docker run --rm -it pybombs/pybombs-$img:latest pybombs --version | tr -d '\r'`
-    docker tag pybombs/pybombs-$img:latest pybombs/pybombs-$img:$pbversion
-    echo -e "\e[1;36mSuccessfully built pybombs/pybombs-$img (tagged latest and $pbversion).\e[0m"
-    cd ..
-    echo ""
-done
+cmd=${1-usage}
+org=${org-pybombs}
+default_images="pybombs-minimal pybombs-prefix pybombs-commondeps"
 
-echo -e "\e[1;36mCreated the following images:\e[0m"
-docker images | grep pybombs/pybombs
+case $cmd in
+    build )
+        images=${2-$default_images}
+        for img in $images; do
+                echo -e "\e[1;36mBuilding $org/$img:latest ...\e[0m"
+                cd $img
+               docker build --build-arg makewidth=`nproc` -t $org/$img:latest .
+                pbversion=`docker run --rm -it $org/$img:latest pybombs --version | tr -d '\r'`
+                docker tag $org/$img:latest $org/$img:$pbversion
+                echo -e "\e[1;36mSuccessfully built pybombs/pybombs-$img (tagged latest and $pbversion).\e[0m"
+                cd ..
+                echo ""
+        done
+        echo -e "\e[1;36mCreated the following images:\e[0m"
+        docker images | grep $org/
+        ;;
+    push )
+        img=${2-_}
+        tags=${3-_}
+        if [[ $img = "_" ]]; then
+            echo "${0} ${1} requires an image name passed as an argument."
+            exit 1
+        fi
+        if [[ $tags = "_" ]]; then
+            tags=`docker images --format "{{.Repository}}:{{.Tag}}" | grep "$org/$img" | cut -d':' -f2`
+        fi
+        for tag in $tags; do
+                echo -e "\e[1;36mPushing $org/$img:$tag ...\e[0m"
+            docker push "$org/$img:$tag"
+        done
+        ;;
+    update )
+        images=${2-$default_images}
+        echo -e "\e[1;36mBuild $images ...\e[0m"
+        for img in $images; do
+            ${0} build $img
+        done
+        echo -e "\e[1;36mPush $images ...\e[0m"
+        for img in $images; do
+            ${0} push $img
+        done
+        ;;
+    clean )
+        echo -e "\e[1;36mCleaning up ...\e[0m"
+        rmi=`docker images --format "{{.Repository}}:{{.Tag}}" | grep "$org/"`
+        for r in $rmi; do
+            docker rmi $r && echo -e "\e[1;36mRemoved $r.\e[0m"
+        done
+        echo -e "\e[1;36mPrune images?\e[0m"
+        read -p "y / n " yn
+        case $yn in
+            [Yy]* ) yes | docker image prune;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+        ;;
+    * )
+        cat <<USAGE
+Usage:
+./build.sh <command> [...]
+
+Commands:
+    - build [<image>]       Builds <image>, or all images if no argument is 
+                            provided.
+    - push <image> [<tag>]  Pushes <image>:<tag>, or all available tags if no
+                            argument is provided.
+    - update [<image>]      Builds and pushes <image> with all current tags,
+                            or all images if no <image> is provided.
+    - clean                 Remove local ${org}/* images.
+    - usage                 Show this message.
+
+USAGE
+
+esac
+
+exit
+
